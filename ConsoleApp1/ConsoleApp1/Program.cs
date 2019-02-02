565f59ef271c190f6d374b41710f3425715122f0
aamir <wasi3aamir@gmail.com>
2019-02-02 22:08:12 +0600

    Unsolved

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int T = int.Parse(Console.ReadLine());
+            for(int t = 0; t < T; t++)
+            {
+                string line = Console.ReadLine();
+                string[] space = line.Split(' ');
+                double num1 = double.Parse(space[0]);
+                double num2 = double.Parse(space[1]);
+                double a = (2.00 * num1);
+                double Dc = Math.Round(a,2.00);
+                double Sc = Math.Round(num2*Math.Sqrt(2),2.00);
+                //Console.WriteLine(Dc);
+               //Console.WriteLine(Sc);
+                if (Dc == Sc)
+                {
+                    Console.WriteLine("Yes");
+                }
+                else
+                {
+                    Console.WriteLine("No");
+                }
+            }
+        }
+    }
+}
---
+++ ConsoleApp2/ConsoleApp2/Animal.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Animal
+    {
+        private string name;
+        private string sound;
+
+        public void MakeSound()
+        {
+            Console.WriteLine($"{name} says {sound}");
+        }
+        public Animal()
+            : this("No Name", "No SOund") { }
+        public Animal(string name)
+            :this(name,"No Sound") { }
+        public Animal(string name,string sound)
+        {
+            
+        }
+    }
+}
